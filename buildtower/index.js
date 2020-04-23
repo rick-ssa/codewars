99c8823fed6be3b6979aa351ab58b9ab5ff7539a
rick-ssa <rick_ssa@hotmail.com>
2020-04-23 01:52:11 -0300

    build tower function

---
+++ buildtower/index.js
@@ -0,0 +1,24 @@
+function towerBuilder(nFloors) {
+  const base = nFloors * 2 - 1
+  const tower = []
+  let whiteSpace = ''
+  let astheristic = ''
+
+  for(let i=0; i<nFloors;i++) {
+    astheristic = repeatCharacter('*',(i + 1) * 2 - 1)
+    whiteSpace = repeatCharacter(' ', (base - astheristic.length)/2)
+    tower.push(whiteSpace + astheristic + whiteSpace)
+  }
+
+  return tower
+}
+
+function repeatCharacter(char, num) {
+  let result = ''
+  for (let i=0; i<num; i++) {
+    result += char
+  }
+  return result
+}
+
+console.log(towerBuilder(7))
